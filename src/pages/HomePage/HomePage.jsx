eb67d402acdff5d4e4919851687a954cbf739126
Sheyla <sheylarellanopsico@gmail.com>
2025-03-11 16:25:10 +0100

    Cambios varios

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,10 +8,15 @@ import ColaboracionSlider from "../../components/ColaboracionSlider/Colaboracion
 import CircularText from "../../components/CircularText/CircularText";
 import AboutMe from "../../assets/images/aboutImg.jpg";
 import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Aurora from "../../components/Aurora/Aurora";
+import GradientText from "../../components/ReservaButton/GradientText";
+import giftCard1 from "../../assets/images/giftCard1.jpeg";
+import giftCard2 from "../../assets/images/giftCard2.jpeg";
+import giftCard3 from "../../assets/images/giftCard3.jpeg";
+import giftCard4 from "../../assets/images/giftCard4.jpeg";
+import { Container, Grid2 } from "@mui/material";
 
 function HomePage() {
   const auroraStyle = {
@@ -27,7 +32,7 @@ function HomePage() {
         <div style={{ position: "relative", width: "100%", height: "100vh" }}>
           <Aurora
             style={auroraStyle} // Pasar el estilo al componente
-            colorStops={["#E6E6FA", "#FF94B4", "#FF3232"]}
+            colorStops={["#f3d2d2", "#FF94B4", "#9E2A2A"]}
             blend={0.5}
             amplitude={0.9}
             speed={0.5}
@@ -36,38 +41,15 @@ function HomePage() {
         <div className="home-content">
           <h1>HUN NAILS</h1>
           <h3 className="roboto-font">STUDIO</h3>
-          <h4 className="roboto-font">Nail Art Studio en Barcelona</h4>
-          <Button
-            component={Link}
-            to="/reserva"
-            sx={{
-              marginTop: "1.2rem",
-              border: "2.5px solid #800000",
-              borderRadius: "10px",
-              color: "#800000",
-              width: {
-                xs: "38.5%",
-                md: "5rem",
-              },
-              padding: "0.2rem 3.5rem",
-              backgroundColor: "transparent",
-              fontSize: "1rem",
-              fontWeight: "bolder",
-              textTransform: "capitalize",
-              cursor: "pointer",
-              transition: "color 0.5s ease, transform 0.3s ease",
-              ":hover": {
-                color: "#800000",
-                background: "white",
-                transform: "translateY(-4px)",
-              },
-              ":active": {
-                transform: "translateY(-1px)",
-              },
-            }}
+          <h4 className="roboto-font">Nail Art en Barcelona</h4>
+          <GradientText
+            colors={["#800000", "#C97575", "#800000", "#C97575", "#800000"]}
+            animationSpeed={6}
+            showBorder={false}
+            className="reserva-button roboto-font"
           >
             Reserva
-          </Button>
+          </GradientText>
         </div>
       </div>
       <div className="galeria-section">
@@ -91,7 +73,20 @@ function HomePage() {
       <div className="pressOn-section">
         <PressOnSection />
       </div>
-      
+      <div className="giftCard-section">
+      <h2>Gift Card</h2>
+        <Grid2 container columns={2} spacing={2} justifyContent={"center"}>
+          <img src={giftCard1} alt="gift card 1"/>
+          <img src={giftCard2} alt="gift card 2" />
+          <img src={giftCard3} alt="gift card 3" />
+          <img src={giftCard4} alt="gift card 4" />
+        </Grid2>
+        <Container className="giftCard-text roboto-font">
+          <h3>¿Quieres regalar algo especial?</h3>
+          <p>Regala una experiencia a tus amigues, pareja o familiares.</p>
+        </Container>
+      </div>
+
       <div className="about-section">
         <h2>HUN Studio</h2>
         <img
