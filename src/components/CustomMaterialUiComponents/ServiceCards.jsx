41eb63f0a612bd5e944aea3248d5b95b9c525fc5
Sheyla <sheylarellanopsico@gmail.com>
2025-03-13 12:47:50 +0100

    Cambios varios sheyla

--- src/components/CustomMaterialUiComponents/ServiceCards.jsx
+++ src/components/CustomMaterialUiComponents/ServiceCards.jsx
@@ -2,21 +2,22 @@ import { Container } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 import workshop from '../../assets/images/Taller-CryBaby.jpg';
+import nailart from '../../assets/images/nailartimg.jpg';
 
 
 function ServiceCards() {
   const services = [
-    { id: 1, src: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngkit.com%2Fpng%2Fdetail%2F495-4958054_vintage-cat-png-clipart-cat-and-kitten-cat.png&f=1&nofb=1&ipt=8020ec2f4228972eedf79b289eda95ad8142fc983d73db30fb2c53b6339fefbc&ipo=images", titulo: "Nail Art", descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus suscipit enim, vel viverra elit malesuada et.", link: "/nailart" },
+    { id: 1, src: nailart, titulo: "Nail Art", descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus suscipit enim, vel viverra elit malesuada et.", link: "/nailart" },
     { id: 2, src: workshop, titulo: "Workshops", descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus suscipit enim, vel viverra elit malesuada et.", link: "/workshops" },
   ];
 
   return (
-    <Container style={{ padding: "0 2.5rem" }}>
+    <Container style={{ padding: "0 0.2rem" }}>
       {services.map((eachService) => (
         <div key={eachService.id}>
           <img
             src={eachService.src}
-            style={{ width: "7.5rem", height: "7.5rem", borderRadius: "50%" }}
+            style={{ width: "100%", height: "auto"}}
             alt={eachService.titulo}
           />
           <Link to={eachService.link} style={{ textDecoration: 'none', color: 'inherit' }}>
--- src/pages/NailArtPage/NailArtPage.jsx
+++ src/pages/NailArtPage/NailArtPage.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./NailArtPage.css";
 import CustomCard from "../../components/CustomCard/CustomCard";
 import Grid2 from '@mui/material/Grid2';
+import nails1 from "../../assets/images/nails1.jpeg";
+import nails2 from "../../assets/images/nails2.jpeg";
+import nails3 from "../../assets/images/nails3.jpeg";
+import nails4 from "../../assets/images/nails4.jpeg";
 import nails6 from "../../assets/images/nails6.jpg";
 import nails7 from "../../assets/images/nails7.jpg";
 import nails8 from "../../assets/images/nails8.jpg";
@@ -12,6 +16,11 @@ const nailArtData = [
   { id: 2, title: "Diseño 2", image: nails7, description: "Descripción del diseño 2" },
   { id: 3, title: "Diseño 3", image: nails8, description: "Descripción del diseño 3" },
   { id: 4, title: "Diseño 4", image: nails9, description: "Descripción del diseño 4" },
+  { id: 5, title: "Diseño 5", image: nails1, description: "Descripción del diseño 5" },
+  { id: 6, title: "Diseño 6", image: nails2, description: "Descripción del diseño 6" },
+  { id: 7, title: "Diseño 7", image: nails3, description: "Descripción del diseño 7" },
+  { id: 8, title: "Diseño 8", image: nails4, description: "Descripción del diseño 8" },
+
 ];
 
 function NailArtPage() {
